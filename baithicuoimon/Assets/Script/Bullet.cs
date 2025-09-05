49b7adebbde66f68665faaa69693c56e87f6e319
ngtuanz <nguyenanhtuan4643@gmail.com>
2025-09-06 00:50:24 +0700

    update lai bai final

---
+++ baithicuoimon/Assets/Script/Bullet.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float lifeTime = 3f;
+    public int damage = 10; // sát thương dành cho Boss
+
+    void Start()
+    {
+        Destroy(gameObject, lifeTime); // tự hủy sau 3s
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Bullet hit: " + other.name + " | Tag: " + other.tag);
+        // Nếu trúng Enemy (Chicken) → chỉ xóa đạn
+        if (other.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+        }
+
+        // Nếu trúng Boss → gây damage
+        if (other.CompareTag("Boss"))
+        {
+            Destroy(gameObject);
+
+            Boss boss = other.GetComponent<Boss>();
+            if (boss != null)
+            {
+                boss.TakeDamage(damage); // Trừ máu Boss
+            }
+        }
+    }
+}
